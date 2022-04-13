524ce554210a961fcacece0607270e2fa9cf9329
hamzashaikh8 <hamzashaikh8@hotmail.com>
2022-04-13 16:13:02 -0400

    edit login.html

--- website/auth.py
+++ website/auth.py
@@ -1,15 +1,19 @@
-from flask import Blueprint, Flask, render_template
+from flask import Blueprint, Flask, render_template, request
 
 auth = Blueprint('auth',__name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET','POST'])
 def login():
-    return render_template("login.html", text="Testing")
+    data = request.form 
+    print(data)
+    return render_template("login.html")
 
 @auth.route('/logout')
 def logout():
     return "<h1>Logout</h1>"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up',methods=['GET','POST'])
 def sign_up():
+    data = request.form 
+    print(data)
     return render_template("sign_up.html")
