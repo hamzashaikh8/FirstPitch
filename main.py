5defd79d93b224195dc6fd7ad06cf9039ae4c049
hamzashaikh8 <hamzashaikh8@hotmail.com>
2022-04-25 01:56:09 -0400

    add error page

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
-from flask_login import LoginManager
+from flask import render_template
+from flask_login import LoginManager, current_user
 from website import create_app
 from website import views
 from website import auth
 
 app = create_app()
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html", user=current_user), 404
 
 if __name__ == '__main__':
     app.run(debug=True)
--- website/views.py
+++ website/views.py
@@ -1,8 +1,10 @@
-from flask import Blueprint, render_template
+from xml.dom.pulldom import ErrorHandler
+from flask import Blueprint, appcontext_popped, render_template
 from flask_login import login_user, login_required, logout_user, current_user
 
 
 views = Blueprint('views', __name__)
+
 
 @views.route('/')
 def about():
